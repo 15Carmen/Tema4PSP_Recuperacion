e5b58d3baaf3354bf2442dda4a8506c11cbd0a0f
15Carmen <carmen.martin@iesnervion.es>
2023-06-18 12:36:59 +0200

    Vale ahora si que si el ejercicio03 está terminado

--- src/rsa/ejercicio03/Encriptar.java
+++ src/rsa/ejercicio03/Encriptar.java
@@ -4,36 +4,42 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.io.*;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.util.Scanner;
+import java.security.*;
+import java.security.interfaces.RSAKey;
+
 
 public class Encriptar {
 
-    public static void encriptarFichero(String ruta){
+    //declaramos una constante con la ruta del fichero a encriptar
+    public static final String RUTA_FICHERO = "src/rsa/ejercicio03/fichero.txt";
+    public static final String RUTA_FICHERO_ENCRIPTADO = "src/rsa/ejercicio03/ficheroEncriptado.txt";
+
+    public static void encriptarFichero(){
 
         //Declaramos los arrays de bytes que vamos a utilizar
-        byte [] mensajeCifradoPrivada;  //array de bytes que contendrá el mensaje cifrado por la clave privada del emisor
-        byte [] mensajeCifradoPublica;  //array de bytes que contendrá el mensaje cifrado por la clave pública del receptor
+        byte [] mensajeCifradoEmisor;  //array de bytes que contendrá el mensaje cifrado por la clave privada del emisor
+        byte [] mensajeCifradoReceptor;  //array de bytes que contendrá el mensaje cifrado por la clave pública del receptor
+        byte[] mensajeOriginal;         //array de bytes que contendrá el mensaje leido en el fichero que se quiere encriptar
 
         try {
-            //Inicializamos los cipher con las claves públicas y privadas
+            //Declaramos las claves pública y privada
             PrivateKey clavePrivadaEmisor = ClavesEmisor.getClavePrivada();
-            Cipher cifradorEmisor = Cipher.getInstance("RSA");
-            cifradorEmisor.init(Cipher.ENCRYPT_MODE, clavePrivadaEmisor);
-
             PublicKey clavePublicaReceptor = ClavesReceptor.getClavePublica();
-            Cipher cifradorReceptor = Cipher.getInstance("RSA");
-            cifradorReceptor.init(Cipher.ENCRYPT_MODE, clavePublicaReceptor);
 
-            //Ciframos el mensaje
-            mensajeCifradoPrivada = cifradorEmisor.doFinal(leerFichero(ruta).readAllBytes());
-            mensajeCifradoPublica = cifradorReceptor.doFinal(mensajeCifradoPrivada);
+            //Declaramos el cifrador con la clave privada del receptor
+            Cipher cipherEmisor = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipherEmisor.init(Cipher.ENCRYPT_MODE, clavePrivadaEmisor);
+
+            //Leemos el fichero encriptado
+            mensajeOriginal = leerFichero().readAllBytes();
+
+            //Encriptamos el fichero primero con la clave privada del emisor
+            mensajeCifradoEmisor = cifrarContenido(mensajeOriginal, clavePrivadaEmisor);
+            //Encriptamos el fichero con la clave pública del receptor
+            mensajeCifradoReceptor = cifrarContenido(mensajeCifradoEmisor, clavePublicaReceptor);
 
             //Guardamos el mensaje cifrado en un fichero
-            guardarFichero(mensajeCifradoPublica);
+            guardarFichero(mensajeCifradoReceptor);
             System.out.println("Mensaje cifrado correctamente");
 
         } catch (NoSuchPaddingException e) { //Si el padding no es válido saltará una excepción
@@ -54,21 +60,49 @@ public class Encriptar {
         } catch (IOException e) { //Si hay un error de lectura del fichero saltará una excepción
             System.err.println("Error de lectura del fichero");
             e.printStackTrace();
+        }catch (Exception e) {  //Si hay un error al encriptar el fichero saltará una excepción
+            System.err.println("Hubo un error al encriptar el fichero");
+            e.printStackTrace();
         }
+    }
+
+    public static byte[] cifrarContenido(byte[] contenido, Key clave) throws Exception {
+
+        // Crear objeto Cipher
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, clave);
+
+        // Calcular tamaño del bloque
+        int tamanoBloque = (((RSAKey)clave).getModulus().bitLength() + 7) / 8 - 11;
+
+        // Inicializar buffer de salida
+        ByteArrayOutputStream bufferSalida = new ByteArrayOutputStream();
+
+        // Cifrar el contenido en bloques
+        int offset = 0;
+        while (offset < contenido.length) {
+            int tamanoBloqueActual = Math.min(tamanoBloque, contenido.length - offset);
+            byte[] bloqueCifrado = cipher.doFinal(contenido, offset, tamanoBloqueActual);
+            bufferSalida.write(bloqueCifrado);
+            offset += tamanoBloqueActual;
+        }
+
+        // Devolver contenido cifrado completo
+        return bufferSalida.toByteArray();
     }
 
     /**
      * Método que lee un fichero y lo devuelve como un FileInputStream
      * @return Datos del fichero en un FileInputStream
      */
-    private static FileInputStream leerFichero (String ruta){
+    private static FileInputStream leerFichero (){
 
         //Declaramos un FileInputStream para leer el fichero
         FileInputStream fileInputStream = null;
 
         try {
             //Inicializamos el FileInputStream con la ruta del fichero
-            fileInputStream = new FileInputStream(ruta);
+            fileInputStream = new FileInputStream(RUTA_FICHERO);
 
         } catch (FileNotFoundException e) { //Si el fichero no existe saltará una excepción
             System.err.println("Fichero no encontrado");
@@ -87,7 +121,7 @@ public class Encriptar {
 
         try {
             //Inicializamos un FileOutputStream con la ruta del fichero
-            FileOutputStream fileOutputStream = new FileOutputStream("src/rsa/ejercicio03/ficheroEncriptado.txt");
+            FileOutputStream fileOutputStream = new FileOutputStream(RUTA_FICHERO_ENCRIPTADO);
 
             //Escribimos el array de bytes en el fichero
             fileOutputStream.write(mensajeCifrado);
--- src/rsa/ejercicio03/Main.java
+++ src/rsa/ejercicio03/Main.java
@@ -1,17 +1,12 @@
 package rsa.ejercicio03;
 
 import java.security.KeyPair;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         //Declaramso las variables
-        int opcion; //Opción del menú
-
-        //Declaramos el scanner para leer la opción del menú
-        Scanner sc = new Scanner(System.in);
 
         //Generamos las claves pública y privada del emisor y del receptor
         KeyPair clavesEmisor = ClavesEmisor.generarClavesEmisor();
@@ -21,56 +16,10 @@ public class Main {
         ClavesEmisor.guardarClaves(clavesEmisor);
         ClavesReceptor.guardarClaves(clavesReceptor);
 
-        do { //Mientras el usuario no elija la opción de salir
+        //Ciframos el fichero
+        Encriptar.encriptarFichero();
 
-            //Mostramos el menú y leemos la opción elegida por el usuario
-            menu();
-            opcion = sc.nextInt();
-
-            switch (opcion){
-                case 1->{ //Cifrar
-
-                    //Declaramos las variables
-                    String rutaFichero; //Ruta del fichero a cifrar
-
-                    //Pedimos la ruta del fichero a cifrar
-                    System.out.println("Introduce la ruta del fichero a cifrar:");
-                    rutaFichero = sc.next();
-
-                    //Ciframos el fichero
-                    Encriptar.encriptarFichero(rutaFichero);
-                }
-
-                case 2->{ //Descifrar
-
-                    //Declaramos las variables
-                    String rutaFichero; //Ruta del fichero a descifrar
-
-                    //Pedimos la ruta del fichero a descifrar
-                    System.out.println("Introduce la ruta del fichero a descifrar:");
-                    rutaFichero = sc.next();
-
-                    Descifrar.descifrarFichero(rutaFichero);
-                }
-
-                case 3->{ //Salir
-                    System.out.println("Adios!");
-                }
-
-                default -> System.out.println("Opción no válida");
-            }
-
-
-        }while (opcion != 3);
-
-        //Cerramos el scanner
-        sc.close();
-    }
-
-    private static void menu(){
-        System.out.println("Elige una opción:");
-        System.out.println("[1] Cifrar fichero");
-        System.out.println("[2] Descifrar fichero");
-        System.out.println("[3] Salir");
+        //Desciframos el fichero
+        Descifrar.descifrarFichero();
     }
 }
